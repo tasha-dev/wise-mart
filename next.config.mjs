a63628bd846390a4b0bc378bebae00ae99ecc1c6
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-06-26 08:46:38 +0330

    added comments to some files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+// Codes by mahdi tasha
+// Importing part
 /** @type {import('next').NextConfig} */
+
+// Defining configs of next.js
 const nextConfig = {};
 
+// Exporting the configs
 export default nextConfig;
